274afce1b594d158eab13e6d313f4ca04d87fb16
Xuhui <lixuhuihui@icloud.com>
2024-05-20 21:54:46 +0800

    modified

--- test_pose_graph.py
+++ test_pose_graph.py
@@ -7,7 +7,8 @@ from src.utils.pose_graph_utils import dense_surface_alignment
 points_1 = torch.empty(10, 3)
 points_2 = torch.empty(10, 3)
 
-rotation = Rotation.from_euler('xyz', [1.5, -2, 0.5], degrees=True)
+Rot = Rotation.from_euler('xyz', [1.5, -2, 0.5], degrees=True)
+rotation = torch.Tensor(Rot.as_matrix())
 translation = torch.Tensor((1, 2, 1))
 relative_pose = torch.eye(4)
 relative_pose[:3, :3] = rotation
