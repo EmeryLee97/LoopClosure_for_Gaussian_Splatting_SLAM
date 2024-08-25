bcdf85565ad640dff4169a289958cfb2a1aab693
Xuhui <lixuhuihui@icloud.com>
2024-08-25 20:06:28 +0200

    modified

--- src/entities/gaussian_slam.py
+++ src/entities/gaussian_slam.py
@@ -181,7 +181,7 @@ class GaussianSLAM(object):
                         # TODO: torch.cuda.empty_cache()?
                         del gaussian_model_prev
                         # modify the poses in one submap TODO: interpolation?
-                        pose_val.to('cpu')
+                        pose_val.to(self.estimated_c2ws[frame_idx].device)
                         for frame_idx in range(submap_start_idx, submap_end_idx+1):
                             self.estimated_c2ws[frame_idx] = pose_val[:3, :3] @ self.estimated_c2ws[frame_idx] + pose_val[:3, 3]
                         # reinitialize for next optimization
