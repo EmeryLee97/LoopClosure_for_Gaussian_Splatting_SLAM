28190f2c55d5650638605d7e6ea4bcd3caba5e1f
Xuhui <lixuhuihui@icloud.com>
2024-08-28 00:11:06 +0200

    modified

--- src/utils/pose_graph_utils.py
+++ src/utils/pose_graph_utils.py
@@ -112,7 +112,7 @@ def error_fn_dense_gaussian_alignment(optim_vars, aux_vars) -> torch.Tensor:
 
     h_distance = hellinger_distance(gaussian_xyz_i_corrected, gaussian_covariance_i, gaussian_xyz_j_corrected, gaussian_covariance_i) # (batch_size, num_gs)
     color_diff = torch.norm(gaussian_color_i.tensor - gaussian_color_j.tensor, p=1, dim=-1) # (batch_size, num_gs)
-    color_diff_sigmoid = modified_sigmoid(color_diff, k=8)
+    color_diff_sigmoid = modified_sigmoid(color_diff, k=10)
     # color_mask = color_diff_sigmoid > 0.2
     # num_valid_pair = color_mask.squeeze().sum()
     # return color_diff_sigmoid * h_distance.sqrt() / num_gs
